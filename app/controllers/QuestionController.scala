ed1fec585e55097de012949d1936dfc065f41be5
Craig Ferguson <cf443@cam.ac.uk>
2017-12-20 00:08:18 +0000

    Adding code documentation in QuestionGenerator and QuestionController.

--- app/controllers/QuestionController.scala
+++ app/controllers/QuestionController.scala
@@ -14,11 +14,15 @@ class QuestionController @Inject() (cc: ControllerComponents,
                                     q: QuestionGenerator) extends AbstractController(cc) {
 
   def generate(lower_bound: Int, upper_bound: Int) = Action {
+
     if (lower_bound < 0)
       BadRequest("ERROR: Lower bound may not be less than 0.")
 
     else if (upper_bound > 1000000)
-      BadRequest("ERROR: Upper bound may not be greater than 1,000,000.")
+    BadRequest("ERROR: Upper bound may not be greater than 1,000,000.")
+
+    else if (lower_bound > upper_bound)
+      BadRequest("ERROR: Lower bound may not be greater than the upper bound.")
 
     else
       Ok(Json.prettyPrint(Json.toJson(q.generateQuestion(lower_bound, upper_bound))))
